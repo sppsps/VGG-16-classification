3a577a870b9f1da26d955f0376d55f31da1b72a0
sppsps <58389597+sppsps@users.noreply.github.com>
2021-06-06 23:32:10 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def vgg_model():
+  model = keras.Sequential()
+  model.add(keras.Input(shape = (32,32,3)))
+  model.add(layers.Conv2D(filters =64, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =64, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)))
+  model.add(layers.Conv2D(filters =128, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =128, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'valid'))
+  model.add(layers.Conv2D(filters =256, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =256, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =256, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =256, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'valid'))
+  model.add(layers.Conv2D(filters =512, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =512, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =512, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =512, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'valid'))
+  model.add(layers.Conv2D(filters =512, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =512, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =512, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.Conv2D(filters =512, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
+  model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'valid'))
+  model.add(layers.Flatten())
+  model.add(layers.BatchNormalization())
+  model.add(layers.Dense(4096, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(0.0005)))
+  model.add(layers.Dropout(0.5))
+  model.add(layers.BatchNormalization())
+  model.add(layers.Dense(4096, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(0.0005),))
+  model.add(layers.Dropout(0.5))
+  model.add(layers.BatchNormalization())
+  model.add(layers.Dense(1000, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(0.0005))) 
+  model.add(layers.BatchNormalization())
+  model.add(layers.Dense(10))
+  model.add(layers.Activation('softmax'))
+  optimizer = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=False, name="SGD",)
+  my_model.compile(
+  optimizer = optimizer,
+  loss = 'categorical_crossentropy',
+  metrics = ['accuracy']
+  )
+  return model
+
---
+++ training.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+history1 = LossHistory()
+earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', verbose=0, mode='min', patience = 4)
+mcp_save = keras.callbacks.ModelCheckpoint('vgg16_1.h5', save_best_only=True, monitor='val_acc', mode='max')
+history = my_model.fit(
+    x_train,y_train,batch_size = 64, epochs = 50, validation_data = (x_test,y_test), callbacks = [mcp_save,earlyStopping]
+)
+
