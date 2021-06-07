525d70a6411a728627d9f8b8cf2a31b8f72a8df2
sppsps <58389597+sppsps@users.noreply.github.com>
2021-06-07 17:30:00 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
-def vgg_model():
+def vgg_model(learning_rate, momentum, regularizer, dropout):
   model = keras.Sequential()
   model.add(keras.Input(shape = (32,32,3)))
   model.add(layers.Conv2D(filters =64, kernel_size=(3,3), strides = (1,1), padding = 'same', activation = 'relu'))
@@ -34,23 +34,27 @@ def vgg_model():
   model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2), padding = 'valid'))
   model.add(layers.Flatten())
   model.add(layers.BatchNormalization())
-  model.add(layers.Dense(4096, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(0.0005)))
-  model.add(layers.Dropout(0.5))
+  model.add(layers.Dense(4096, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(regularizer)))
+  model.add(layers.Dropout(dropout))
   model.add(layers.BatchNormalization())
-  model.add(layers.Dense(4096, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(0.0005),))
-  model.add(layers.Dropout(0.5))
+  model.add(layers.Dense(4096, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(regularizer)))
+  model.add(layers.Dropout(dropout))
   model.add(layers.BatchNormalization())
-  model.add(layers.Dense(1000, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(0.0005))) 
+  model.add(layers.Dense(1000, 'relu', True, kernel_regularizer=tf.keras.regularizers.l2(regularizer))) 
   model.add(layers.BatchNormalization())
   model.add(layers.Dense(10))
   model.add(layers.Activation('softmax'))
-  optimizer = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=False, name="SGD",)
-  my_model.compile(
+  optimizer = tf.keras.optimizers.SGD(learning_rate, momentum, nesterov=False, name="SGD",)
+  model.compile(
   optimizer = optimizer,
   loss = 'categorical_crossentropy',
   metrics = ['accuracy']
   )
   return model
-my_model = vgg_model()
-my_model.summary()
-
+#dummy values for checking values
+learning_rate = 0.01
+momentum = 0.9
+regularizer = 0.0005 
+dropout = 0.5
+dummy_model = vgg_model(learning_rate, momentum, regularizer, dropout)
+dummy_model.summary()
