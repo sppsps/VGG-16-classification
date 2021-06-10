bf0e47a45c65427026e1888a9f7012dfe0826dc4
sppsps <58389597+sppsps@users.noreply.github.com>
2021-06-10 15:01:42 +0530

    Delete loss_plot.py

--- loss_plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.plot(history.history['loss'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test','loss'], loc='upper left')
-plt.show()
